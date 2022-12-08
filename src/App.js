6612f120f86bd474288edcf66db1aa378e82aa7b
Tomiwa <calebomoniyitomiwa@gmail.com>
2022-12-08 13:11:03 +0100

    completed dynamic menu buttons

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ... new Set(items.map((item) => item.category))] ;
+console.log(allCategories)
+
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) =>item.category === category)
+    setMenuItems(newItems)
+  }
   
-  return <main>
+  return (
+  <main>
     <section className="menu section">
       <div className="title">
-        <h1> Our Menus</h1>
+        <h2> Our Menus</h2>
         <div className="underline"></div>
-        <Categories></Categories>
+        <Categories categories = {categories} filterItems = {filterItems}></Categories>
         <Menu items = {menuItems}></Menu>
       </div>
     </section>
   </main>
-}
+  )
+};
 
 export default App;
